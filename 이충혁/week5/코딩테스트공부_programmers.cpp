93ed5df04d1b077a498cbedacd892728d2812efa
unknown <chyuk98@naver.com>
2023-03-15 22:17:07 +0900

    MOD / week5 / 코딩테스트공부(75)

--- 이충혁/week5/코딩테스트공부_programmers.cpp
+++ 이충혁/week5/코딩테스트공부_programmers.cpp
@@ -1,120 +1,129 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 const int MAX = 21e8;
+const int MAX_DP = 200;
 
-// 1. DFS의 기저 조건은 문제들의 최대 알고력 코딩력 이다
-// 3. 가지치기1: 알고력 코딩력의 합보다 얻는게 적다면 수행 하지 않는다.
-
-
-struct Info_limit {
-  int alp, cop;
+struct Info {
+	int alp, cop;
 };
 
 enum PB_CONST {
-  alp_req, cop_req, alp_rwd, cop_rwd, cost
+	alp_req, cop_req, alp_rwd, cop_rwd, cost
 };
 
-Info_limit min_limit, max_limit; // 최소, 최대 알고력 코딩력
+Info max_limit; // 최소, 최대 알고력 코딩력
 int now_time = 0; // dfs 도는데 시간
-int min_time = MAX;
 
-int dfs(int alp, int cop, vector<vector<int>>& problems) // level
-{
-  // 기저 조건
-  if (alp >= max_limit.alp && cop >= max_limit.cop)
-  {
-    if (now_time < min_time)
-      min_time = now_time;
-    return 0;
-  }
 
-  for (int i = 0; i < problems.size(); i++)
-  {
-    if (problems[i][alp_rwd] + problems[i][cop_rwd] <= problems[i][cost]) // 문제 코스트 대비 얻는 리워드가 적을 때
-      continue;
 
-    if (problems[i][alp_req] > alp || problems[i][cop_req] > cop) // 아직 요구조건이 충족 안될때
-      continue;
 
-    now_time += problems[i][cost];
 
-    dfs(alp + problems[i][alp_rwd], cop + problems[i][cop_rwd], problems);
+int DP[MAX_DP][MAX_DP] = { 0, }; // alp, cop
 
-    now_time -= problems[i][cost];
-  }
+int solution(int alp, int cop, vector<vector<int>>& problems) {
 
-  now_time += 1;
+	int ans = MAX;
 
-  if(alp < max_limit.alp)
-    dfs(alp + 1, cop, problems);
+	// 알고력, 코딩력의 최대 최소를 구함
+	for (int i = 0; i < problems.size(); i++)
+	{
+		if (max_limit.alp < problems[i][alp_req])
+			max_limit.alp = problems[i][alp_req];
 
-  if (cop < max_limit.cop)
-    dfs(alp, cop + 1, problems);
+		if (max_limit.cop < problems[i][cop_req])
+			max_limit.cop = problems[i][cop_req];
+	}
 
-  now_time -= 1;
+	// init 
+	for (int i = 0; i < MAX_DP; i++) // alp
+	{
+		for (int j = 0; j < MAX_DP; j++) // cop
+		{
+			DP[i][j] = MAX;
+		}
+	}
 
-  return 0;
+	DP[alp][cop] = 0;
+
+	for (int i = alp; i <= max_limit.alp; i++) // alp
+	{
+		for (int j = cop; j <= max_limit.cop; j++) // cop
+		{
+
+
+			
+
+
+			DP[i + 1][j] = min(DP[i + 1][j], DP[i][j] + 1);
+			DP[i][j + 1] = min(DP[i][j + 1], DP[i][j] + 1);
+
+			for (int k = 0; k < problems.size(); k++)
+			{
+
+				if (i < problems[k][alp_req] || j < problems[k][cop_req])
+					continue;
+
+				int alp_index = i + problems[k][alp_rwd];
+				int cop_index = j + problems[k][cop_rwd];
+
+				if (alp_index > max_limit.alp)
+				{
+					alp_index = max_limit.alp;
+				}
+				if(cop_index > max_limit.cop)
+				{
+					cop_index = max_limit.cop;
+				}
+
+				DP[alp_index][cop_index] = min(DP[alp_index][cop_index], DP[i][j] + problems[k][cost]);
+
+			}
+		}
+	}
+
+	if (DP[max_limit.alp][max_limit.cop] == MAX)
+		DP[max_limit.alp][max_limit.cop] = 0;
+	return DP[max_limit.alp][max_limit.cop];
 }
 
-int solution(int alp, int cop, vector<vector<int>> &problems) {
-  int answer = 0;
-  
-  // init 
-  min_limit = { MAX, MAX };
-  max_limit = { 0, 0 };
-
-  // 알고력, 코딩력의 최대 최소를 구함
-  for (int i = 0; i < problems.size(); i++)
-  {
-    if (max_limit.alp < problems[i][alp_req])
-      max_limit.alp = problems[i][alp_req];
-
-    if (max_limit.cop < problems[i][cop_req])
-      max_limit.cop = problems[i][cop_req];
-  }
-
-  dfs(alp, cop, problems);
-  answer += min_time;
-
-  return answer;
-}
 
 
 // 여기서부터는 입출력용
 
-int input(int &alp, int &cop, vector<vector<int>> &temp_pb)
+int input(int& alp, int& cop, vector<vector<int>>& temp_pb)
 {
-  cin >> alp;
-  cin >> cop;
+	cin >> alp;
+	cin >> cop;
 
-  int size;
-  cin >> size;
+	int size;
+	cin >> size;
 
-  int temp[5];
-  for (int i = 0; i < size; i++)
-  {
-    for (int j = 0; j < 5; j++)
-      cin >> temp[j];
+	int temp[5];
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < 5; j++)
+			cin >> temp[j];
 
-    temp_pb.push_back({ temp[0], temp[1], temp[2], temp[3], temp[4] });
-  }
+		temp_pb.push_back({ temp[0], temp[1], temp[2], temp[3], temp[4] });
+	}
 
-  return 0;
+	return 0;
 }
 
 int main()
 {
-  freopen("input.txt", "r", stdin);
+	freopen("input.txt", "r", stdin);
 
 
-  int alp, cop ;
-  vector<vector<int>> temp_pb;
-  input(alp, cop, temp_pb);
-  cout << solution(alp, cop, temp_pb);
+	int alp, cop;
+	vector<vector<int>> temp_pb;
+	input(alp, cop, temp_pb);
+	cout << solution(alp, cop, temp_pb);
 
-  return 0;
+	return 0;
 }
