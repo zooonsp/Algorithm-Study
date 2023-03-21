eb0b290823ca05342f49b14592a907e539150334
soybean33 <80668684+soybean33@users.noreply.github.com>
2023-03-21 14:43:42 +0900

    Update 5650 [모의 SW 역량테스트] 핀볼 게임.cpp

--- 김용우/week6/5650 [모의 SW 역량테스트] 핀볼 게임.cpp
+++ 김용우/week6/5650 [모의 SW 역량테스트] 핀볼 게임.cpp
@@ -9,10 +9,10 @@ int ans;	// ans: 정답
 int dirY[] = { 0, 1, 0, -1 };
 int dirX[] = { 1, 0, -1, 0 };
 
-void func(int startY, int startX, int curY, int curX, int dir, int depth, int score) {	// 시작위치 y, x, 현재 위치 y, x, 방향 dir, 깊이, 점수
+void func(int startY, int startX, int curY, int curX, int dir, int depth, int score) {	// 시작 위치 y, x, 현재 위치 y, x, 방향 dir, 깊이, 점수
 
 	while (1) {
-		if (depth != 0) {	// 처음에 시작할 때 시작위치 현재 위치인 것을 거르기 위함
+		if (depth != 0) {	// 처음에 시작할 때 시작 위치 현재 위치인 것을 거르기 위함
 			if (startY == curY && startX == curX) {	// 시작 위치로 돌아옴
 				ans = std::max(ans, score);
 				return;
