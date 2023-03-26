1723a02e09dd37c3944ad3482957b908247922b9
profornnan <profornnan@naver.com>
2023-03-26 11:55:14 +0900

    UPDATE / week7 / 5052-전화번호 목록 주석 작성 Trie의 find 함수 구현

--- 탁성건/week7/5052-전화번호 목록.cpp
+++ 탁성건/week7/5052-전화번호 목록.cpp
@@ -5,10 +5,11 @@
 #include <vector>
 using namespace std;
 
+// Trie에서 사용할 Node 구조체
 struct Node {
-  bool isEnd;
-  Node *child[10];
-  Node() {
+  bool isEnd;      // 전화번호의 끝인가?
+  Node *child[10]; // child node를 가리킬 포인터 배열(0~9)
+  Node() {         // 생성자를 이용한 초기화
     isEnd = false;
     memset(child, 0, sizeof(child));
   }
@@ -16,27 +17,44 @@ struct Node {
 
 class Trie {
 private:
-  Node head;
+  Node head; // Trie의 최상단 Node
 
 public:
   bool insert(string str) {
-    Node *ptr = &head;
+    Node *ptr = &head; // Node를 가리킬 포인터. head를 가리키도록 초기화
 
+    // 전화번호를 처음부터 끝까지 확인하며
     for (int i = 0; i < str.size(); i++) {
       int num = str[i] - '0';
 
-      if (ptr->child[num] == nullptr)
+      if (ptr->child[num] == nullptr) // num에 해당하는 child가 없다면 생성
         ptr->child[num] = new Node();
 
-      if (ptr->isEnd)
+      if (ptr->isEnd) // 이미 해당 전화번호로 끝난 경우가 있다면 false
         return false;
 
-      ptr = ptr->child[num];
+      ptr = ptr->child[num]; // ptr이 child node를 가리키도록 변경
     }
 
-    ptr->isEnd = true;
+    ptr->isEnd = true; // 전화번호의 끝 표시
 
     return true;
+  }
+
+  bool find(string str) {
+    Node *ptr = &head; // Node를 가리킬 포인터. head를 가리키도록 초기화
+
+    // 전화번호를 처음부터 끝까지 확인하며
+    for (int i = 0; i < str.size(); i++) {
+      int num = str[i] - '0';
+
+      if (ptr->child[num] == nullptr) // num에 해당하는 child가 없다면 false
+        return false;
+
+      ptr = ptr->child[num]; // ptr이 child node를 가리키도록 변경
+    }
+
+    return ptr->isEnd; // 전화번호의 끝인가?를 return
   }
 };
 
@@ -56,11 +74,11 @@ int main(void) {
     for (int i = 0; i < N; i++)
       cin >> phone[i];
 
-    sort(phone.begin(), phone.end());
+    sort(phone.begin(), phone.end()); // 오름차순 정렬
 
     for (int i = 0; i < N; i++) {
-      if (!trie.insert(phone[i])) {
-        ans = "NO";
+      if (!trie.insert(phone[i])) { // 접두어가 있다면
+        ans = "NO";                 // 일관성 있는 목록이 아니다.
         break;
       }
     }
