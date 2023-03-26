9e17eb4a63c2214169a919c780fcfb03b3f9319a
profornnan <profornnan@naver.com>
2023-03-26 13:03:10 +0900

    UPDATE / week7 / 5653-줄기세포배양 주석 작성

--- 탁성건/week7/5653-줄기세포배양.cpp
+++ 탁성건/week7/5653-줄기세포배양.cpp
@@ -4,11 +4,13 @@
 #define sws ios::sync_with_stdio(false), cin.tie(NULL)
 using namespace std;
 
+// 줄기 세포의 정보를 담을 구조체
+// PQ에서 작은 시간 우선, 높은 생명력 우선
 struct Node {
   int y;
   int x;
-  int vitality;
-  int time;
+  int vitality; // 생명력
+  int time;     // 번식하는 시간
   bool operator<(Node next) const {
     if (time < next.time)
       return false;
@@ -28,30 +30,33 @@ int dy[]{0, -1, 0, 1};
 const int SIZE = 500;
 int N, M, K;
 int MAP[SIZE][SIZE];
-int cnt;
+int cnt; // K시간 이후에 살아있는 번식을 마친 줄기 세포 수
 priority_queue<Node> pq;
 
 void simulation() {
-  for (int k = 1; k <= K; k++) {
+  for (int k = 1; k <= K; k++) { // 1시간 ~ K시간
     while (true) {
-      if (pq.empty())
+      if (pq.empty()) // 줄기 세포가 없으면 break
         break;
-      if (pq.top().time > k)
+      if (pq.top().time > k) // 현재 시간에 번식하는 줄기 세포가 없으면 break
         break;
 
-      Node now = pq.top();
+      Node now = pq.top(); // 지금 번식하는 줄기 세포
       pq.pop();
 
+      // 현재 줄기 세포가 K시간 이후에 살아있다면 cnt 증가
       if (k + now.vitality - 1 > K)
         cnt++;
 
+      // 번식
       for (int i = 0; i < 4; i++) {
         int ny = now.y + dy[i];
         int nx = now.x + dx[i];
 
-        if (MAP[ny][nx] != 0)
+        if (MAP[ny][nx] != 0) // 이미 줄기 세포가 존재하는 경우 번식 X
           continue;
 
+        // 다음 줄기 세포가 번식하는 시간은 현재 시간(k) + 생명력 + 1
         pq.push({ny, nx, now.vitality, now.vitality + k + 1});
         MAP[ny][nx] = now.vitality;
       }
@@ -69,6 +74,7 @@ int main(void) {
     pq = priority_queue<Node>();
 
     cin >> N >> M >> K;
+    // 초기 상태 크기에 맞게 MAP 중앙에 위치시킬 수 있는 Offset 계산
     int yOff = (SIZE - N) / 2;
     int xOff = (SIZE - M) / 2;
 
@@ -77,14 +83,17 @@ int main(void) {
         int ny = y + yOff;
         int nx = x + xOff;
 
-        cin >> MAP[ny][nx];
+        cin >> MAP[ny][nx]; // MAP 중앙에 초기 상태 입력
         if (MAP[ny][nx] != 0)
+          // 줄기 세포라면 PQ에 push
+          // 번식하는 시간은 생명력 + 1
           pq.push({ny, nx, MAP[ny][nx], MAP[ny][nx] + 1});
       }
     }
 
     simulation();
 
+    // PQ에 남아있는 줄기 세포 수 + K시간 이후 살아있는 번식을 마친 줄기 세포 수
     cout << '#' << tc << ' ' << pq.size() + cnt << '\n';
   }
 
