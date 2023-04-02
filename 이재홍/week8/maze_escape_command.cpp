a27f7e6134d49c7043d48773f7500919a2e42fcd
h78749891 <h78749891@gmail.com>
2023-04-02 14:49:21 +0900

    MOD / week8 / 미로 탈출 명령어 (주석 추가)

--- 이재홍/week8/maze_escape_command.cpp
+++ 이재홍/week8/maze_escape_command.cpp
@@ -10,32 +10,31 @@ struct Coordinate {
     Coordinate operator+(const Coordinate &pos) {return {x + pos.x, y + pos.y};}
 };
 
-Coordinate d[DIR] = {{ 1, 0}, { 0,-1}, { 0, 1}, {-1, 0}};
-string s = "dlru";
+Coordinate d[DIR] = {{ 1, 0}, { 0,-1}, { 0, 1}, {-1, 0}}; // direction array
+string s = "dlru"; // order by dictionary
 
-bool out(int n, int m, Coordinate &pos) {
+bool out(int n, int m, Coordinate &pos) { // return if pos is out of map from (1,1) to (n,m)
     return pos.x < 1 || pos.y < 1 || n < pos.x || m < pos.y;
 }
 
-int get_len(Coordinate &pos1, Coordinate &pos2) {
+int get_len(Coordinate &pos1, Coordinate &pos2) { // get minimum route length from pos1 to pos2
     return abs(pos1.x - pos2.x) + abs(pos1.y - pos2.y);
 }
 
 string solution(int n, int m, int x, int y, int r, int c, int k) {
-    string answer = "";
-    Coordinate now = {x, y};
-    Coordinate end = {r, c};
-    int len = get_len(now, end);
+    string answer = ""; // initiate answer string
+    Coordinate now = {x, y}; // current position
+    Coordinate end = {r, c}; // destination
+    int len = get_len(now, end); // get minimum route length from start to destination
     if(k < len || ((k ^ len) & 1)) {return "impossible";} // can't reach (length, odd-even)
-    for(int iter = 1; iter <= k; ++iter) {
-        for(int dir = 0; dir < DIR; ++dir) {
+    for(int iter = 1; iter <= k; ++iter) { // gready approach
+        for(int dir = 0; dir < DIR; ++dir) { // select fastest available direction in dictionary order
             Coordinate next = now + d[dir];
-            if(k - iter < get_len(next, end) || out(n, m, next)) {continue;}
+            if(k - iter < get_len(next, end) || out(n, m, next)) {continue;} // destination not reachable, out of map
             now = next;
             answer += s[dir];
-            break;
+            break; // s[dir] is fastest available character
         }
     }
-
     return answer;
 }
