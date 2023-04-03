b34ae982a6473e438c15d1856490a44770b967e9
profornnan <profornnan@naver.com>
2023-04-03 23:46:01 +0900

    UPDATE / week8 / 5670-휴대폰 자판 주석 작성

--- 탁성건/week8/5670-휴대폰 자판.cpp
+++ 탁성건/week8/5670-휴대폰 자판.cpp
@@ -8,17 +8,17 @@ using namespace std;
 const int SIZE = 26;
 
 struct Node {
-  bool isEnd;
-  int childCnt;
-  Node *child[SIZE];
+  bool isEnd;        // 문자열의 끝인가?
+  int childCnt;      // child의 개수
+  Node *child[SIZE]; // child node를 가리킬 포인터 배열
 
-  Node() {
+  Node() { // 생성자를 이용한 초기화
     isEnd = false;
     childCnt = 0;
     memset(child, 0, sizeof(child));
   }
 
-  ~Node() {
+  ~Node() { // 동적할당 해제
     for (int i = 0; i < SIZE; i++)
       if (child[i] != nullptr)
         delete child[i];
@@ -48,11 +48,13 @@ public:
   }
 
   int find(string str) {
-    Node *ptr = head.child[str[0] - 'a'];
+    Node *ptr = head.child[str[0] - 'a']; // 첫 글자부터 시작
 
-    int pushCnt = 1;
+    int pushCnt = 1; // 첫 글자는 반드시 버튼을 눌러야 한다.
 
+    // 모든 글자를 확인하며
     for (int i = 1; i < str.length(); i++) {
+      // 자식이 2개 이상이거나 다른 문자열의 끝인 경우 버튼을 눌러야 한다.
       if (ptr->childCnt > 1 || ptr->isEnd)
         pushCnt++;
 
@@ -66,6 +68,7 @@ public:
 int main(void) {
   cin.tie(NULL)->sync_with_stdio(false);
 
+  // 소수점 둘째 자리까지 반올림하여 출력
   cout << fixed;
   cout.precision(2);
 
@@ -75,6 +78,7 @@ int main(void) {
     vector<string> dict(N);
     Trie trie;
 
+    // N개의 단어를 Trie에 insert
     for (int i = 0; i < N; i++) {
       cin >> dict[i];
 
@@ -83,9 +87,11 @@ int main(void) {
 
     int sum = 0;
 
+    // N개의 단어를 Trie에서 find하며 버튼을 누른 횟수의 합 구하기
     for (int i = 0; i < N; i++)
       sum += trie.find(dict[i]);
 
+    // 각 단어를 입력하기 위해 버튼을 눌러야 하는 횟수의 평균
     cout << (double)sum / N << '\n';
   }
 
