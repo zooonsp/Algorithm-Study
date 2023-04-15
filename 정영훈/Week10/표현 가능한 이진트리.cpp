5ec36acb6cf16983ed41d12142d232bce3d7a448
ChocoBreeze <ds030337@gmail.com>
2023-04-16 02:46:23 +0900

    MOD / Week10 / 표현 가능한 이진트리(lambda -> function)

--- 정영훈/Week10/표현 가능한 이진트리.cpp
+++ 정영훈/Week10/표현 가능한 이진트리.cpp
@@ -1,8 +1,12 @@
-// lambda
+/*
+1000000000000000
+0011 | 1000 1101 | 0111 1110 | 1010 0100 | 1100 0110 | 1000 0000 | 0000 0000
+해봤자 62개
+*/
+
 #include <string>
 #include <vector>
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
@@ -22,35 +26,44 @@ string MakeBinaryNumber(long long& number) {
 	for (int i = (int)binaryNumber.length(); i < nodeNumber; ++i) {
 		binaryNumber += "0";
 	}
-	//reverse(begin(binaryNumber), end(binaryNumber));
+	// 뒤집어야 진짜 의도한 이진수 이긴 하지만 안 뒤집어도 이후 비교엔 지장 없음.
+	// reverse(begin(binaryNumber), end(binaryNumber));
 	return binaryNumber;
+}
+
+bool Check(string& binaryNumber) {
+	// leaf node
+	vector<int> nodeIndexes;
+	for (int i = 0; i < (int)binaryNumber.length(); i += 2) nodeIndexes.push_back(i);
+	while (nodeIndexes.size() > 1) {
+		vector<int> temp;
+		for (int i{}; i < (int)nodeIndexes.size(); i += 2) {
+			int mid = (nodeIndexes[i] + nodeIndexes[i + 1]) / 2;
+			if (binaryNumber[nodeIndexes[i]] == '1' || binaryNumber[nodeIndexes[i + 1]] == '1') {
+				if (binaryNumber[mid] != '1') return false;
+			}
+			temp.push_back(mid);
+		}
+		nodeIndexes.swap(temp);
+	}
+	return true;
 }
 
 vector<int> solution(vector<long long> numbers) {
 	vector<int> answer;
 	for (long long& number : numbers) {
 		string binaryNumber = MakeBinaryNumber(number);
-
-		auto ret = [binaryNumber]() -> bool {
-			// leaf node
-			vector<int> nodeIndexes;
-			for (int i = 0; i < binaryNumber.length(); i += 2) nodeIndexes.push_back(i);
-			while (nodeIndexes.size() > 1) {
-				vector<int> tempEvens;
-				for (int i{}; i < (int)nodeIndexes.size(); i += 2) {
-					int mid = (nodeIndexes[i] + nodeIndexes[i + 1]) / 2;
-					if (binaryNumber[nodeIndexes[i]] == '1' || binaryNumber[nodeIndexes[i+1]] == '1') {
-						if (binaryNumber[mid] != '1') return false;
-					}
-					tempEvens.push_back(mid);
-				}
-				nodeIndexes.swap(tempEvens);
-			}
-			return true;
-		} ();
-
-		if (ret) answer.push_back(1);
+		if (Check(binaryNumber)) answer.push_back(1);
 		else answer.push_back(0);
 	}
 	return answer;
+}
+
+int main() {
+	// vector<long long> numbers{ 7, 42, 5 }; // 1 1 0
+	// vector<long long> numbers{ 63, 111, 95, 58 }; // 1 1 0 1
+	vector<long long> numbers{ 1000000000000000 };
+	vector<int> answer = solution(numbers);
+	for (int& i : answer) cout << i << ' ';
+	return 0;
 }
