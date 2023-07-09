f65f64cd8c6d25e87dcc1921fe8ed3da647c86c7
profornnan <profornnan@naver.com>
2023-07-09 18:58:38 +0900

    UPDATE / week19 / 3665-최종 순위 주석 작성

--- 탁성건/week19/3665-최종 순위.cpp
+++ 탁성건/week19/3665-최종 순위.cpp
@@ -13,9 +13,13 @@ int main(void) {
     int N, M;
     cin >> N;
 
+    // 작년 순위
     vector<int> pastRank(N + 1);
+    // change[a][b]: a팀과 b팀의 순위가 바뀌었는가?
     vector<vector<bool>> change(N + 1, vector<bool>(N + 1));
+    // 상대적 순위 인접 그래프
     vector<vector<int>> adj(N + 1);
+    // 해당 정점에 들어오는 간선의 개수
     vector<int> indegree(N + 1);
 
     for (int i = 1; i <= N; i++)
@@ -23,16 +27,20 @@ int main(void) {
 
     cin >> M;
 
+    // 상대적 순위 변경 정보에 따라 change 갱신
     for (int i = 1; i <= M; i++) {
       int a, b;
       cin >> a >> b;
       change[a][b] = change[b][a] = true;
     }
 
+    // 2개의 팀을 선택하는 모든 경우에 대해
     for (int i = 1; i <= N; i++) {
       for (int j = i + 1; j <= N; j++) {
         auto [a, b] = make_pair(pastRank[i], pastRank[j]);
 
+        // a팀과 b팀의 상대적 순위가
+        // 변경되지 않았으면 그대로, 변경되었으면 반대로
         if (!change[a][b]) {
           adj[a].push_back(b);
           indegree[b]++;
@@ -46,6 +54,7 @@ int main(void) {
     vector<int> rank;
     queue<int> q;
 
+    // 위상정렬
     for (int i = 1; i <= N; i++)
       if (indegree[i] == 0)
         q.push(i);
@@ -63,6 +72,7 @@ int main(void) {
       }
     }
 
+    // N개의 정점이 발견되지 않았다면 순위를 정할 수 없는 경우
     if (rank.size() != N)
       cout << "IMPOSSIBLE";
     else
